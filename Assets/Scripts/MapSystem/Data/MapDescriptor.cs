c6fa4daf7be926c839c917cd1318758f50b046ea
Isaiah Mann <contact@isaiahmann.com>
2017-02-18 16:23:37 -0500

    Building more meta data into the map

---
+++ Assets/Scripts/MapSystem/Data/MapDescriptor.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: Describes a map in pure data that can be loaded in
+ * Usage: [no notes]
+ */
+
+using UnityEngine;
+
+public class MapDescriptor 
+{
+    public string BackgroundSprite
+    {
+        get
+        {
+            return backgroundSprite;
+        }
+    }
+
+    public MapData[,][] Map 
+    {
+        get 
+        {
+            return _map;
+        }
+        set
+        {
+            _map = value;
+        }
+        
+    }
+
+
+    [SerializeField]
+    string backgroundSprite;
+   
+    MapData[,][] _map;
+
+}
